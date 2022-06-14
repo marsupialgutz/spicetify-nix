c7177a8258b7460faa0067a6117ae1510e19e179
marsupialgutz <mars@possums.xyz>
2022-06-13 22:38:07 -0400

    uh

--- package.nix
+++ package.nix
@@ -113,7 +113,7 @@ pkgs.spotify-unwrapped.overrideAttrs (oldAttrs: rec {
       lyric_always_show ${boolToString lyricAlwaysShow } \
       lyric_force_no_sync ${boolToString lyricForceNoSync }
 
-      find CustomApps/ -maxdepth 1 -type d -exec cp -r {} $out/share/spotify/Apps \;
+      find CustomApps/ -maxdepth 1 -type d -exec ln -s {} $out/share/spotify/Apps \;
       ${spicetify} backup apply
     
       cd $out/share/spotify
