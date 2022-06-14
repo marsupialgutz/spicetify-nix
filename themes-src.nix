97a5fece7f9a18e10d79d6f7d98fbf00989cc283
marsupialgutz <mars@possums.xyz>
2022-06-13 21:13:25 -0400

    oh woops

--- themes-src.nix
+++ themes-src.nix
@@ -1,9 +1,8 @@
-let
-  pkgs = import <nixpkgs> {};
-in
-  pkgs.fetchFromGitHub {
-    owner = "morpheusthewhite";
-    repo = "spicetify-themes";
-    rev = "dd7a7e13e0dc7a717cc06bba9ea04ed29d70a30e";
-    sha256 = "v6KRTeXYgsL6bVSAY7qRjBCnKKELe9SSx7FVNRkbiRU=";
-  }
+{ fetchFromGitHub }:
+
+fetchFromGitHub {
+  owner = "morpheusthewhite";
+  repo = "spicetify-themes";
+  rev = "dd7a7e13e0dc7a717cc06bba9ea04ed29d70a30e";
+  sha256 = "v6KRTeXYgsL6bVSAY7qRjBCnKKELe9SSx7FVNRkbiRU=";
+}
